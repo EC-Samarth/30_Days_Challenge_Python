35bd4426e0382e932967ac3d8b27eab1cee232d0
EC-Samarth <mishrasamarth25@gmail.com>
2025-05-31 23:14:50 +0530

    Add files via upload

---
+++ Day_01.py
@@ -0,0 +1,22 @@
+#Day 1 <Print and Variables>
+
+# print hte hello world in python
+print ("hello world")
+print("This is my first code")
+
+# swap the two values 
+x = 1
+y = 2 
+x,y =y,x
+print("The vlaue of x is :",x)
+print("The vlaue of y is :",y)
+
+# get user input and print it
+a = input("Enter the name :")
+print("Your name is :",a)
+
+# get int and float user input 
+a = int(input("Enter the number :"))
+b = float(input("Enter the number :"))
+print(a)        
+print(b)
